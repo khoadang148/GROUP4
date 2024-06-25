548b294176c528510eb364a0230eb08b23a81e80
khoadang148 <khoapdse173532@fpt.edu.vn>
2024-06-25 09:56:22 +0700

    fix

--- src/App.js
+++ src/App.js
@@ -3,19 +3,17 @@ import Header from "./components/Header";
 import Sidebar from "./components/Sidebar";
 import { Container } from "react-bootstrap";
 import LoginScreen from "./LoginScreen";
+import SignupScreen from "./SignupScreen";
+import ForgotPasswordScreen from "./ForgotPasswordScreen";
 import {
   BrowserRouter as Router,
   Routes,
   Route,
   useNavigate,
-  Redirect,
   Navigate,
 } from "react-router-dom";
 import HomeScreen from "./HomeScreen";
 import Livestream from "./Livestream";
-import { AuthContextProvider, useAuth } from "./context/AuthContext";
-import withAuth from "./AuthChecker";
-import AuthChecker from "./AuthChecker";
 import { useDispatch, useSelector } from "react-redux";
 import ExploreScreen from "./ExploreScreen";
 import Footer from "./components/Footer";
@@ -25,15 +23,6 @@ import DashBoard from "./DashBoard";
 import { setToken, setRole } from "./redux/actions/auth.action";
 import Cookies from "js-cookie";
 
-// const ProtectedRoute = ({ element: Element, ...rest }) => {
-//   const { user } = useAuth();
-
-//   if (!user) {
-//     return <Navigate to="/login" />;
-//   }
-
-//   return <Route {...rest} element={<Element />} />;
-// };
 const Layout = ({ children }) => {
   const [sidebar, toggleSidebar] = useState(true);
   const handleToggleSidebar = () => toggleSidebar(!sidebar);
@@ -43,14 +32,7 @@ const Layout = ({ children }) => {
     <div className="flex flex-col min-h-screen">
       <Header handleToggleSidebar={handleToggleSidebar} />
       <div className="flex text-black">
-        {role === "student" && (
-          <Sidebar
-            sidebar={sidebar}
-            handleToggleSidebar={handleToggleSidebar}
-            className="relative z-50"
-          />
-        )}
-        {role === "teacher" && (
+        {(role === "student" || role === "teacher") && (
           <Sidebar
             sidebar={sidebar}
             handleToggleSidebar={handleToggleSidebar}
@@ -59,9 +41,7 @@ const Layout = ({ children }) => {
         )}
         <Container
           fluid
-          className={`transform duration-700  ${
-            sidebar ? "ml-[250px]" : "ml-0 "
-          }`}
+          className={`transform duration-700  ${sidebar ? "ml-[250px]" : "ml-0 "}`}
         >
           {children}
         </Container>
@@ -84,15 +64,12 @@ const App = () => {
   const navigate = useNavigate();
 
   useEffect(() => {
-    // Đọc token từ cookie
     const tokenFromCookie = Cookies.get("token");
     if (tokenFromCookie) {
       dispatch(setToken(tokenFromCookie));
       dispatch(setRole(tokenFromCookie));
-    } else {
-      navigate("/login");
     }
-  }, [dispatch, navigate]);
+  }, [dispatch]);
 
   useEffect(() => {
     if (token && role === "teacher") {
@@ -103,6 +80,8 @@ const App = () => {
   return (
     <Routes>
       <Route path="/login" element={<LoginScreen />} />
+      <Route path="/signup" element={<SignupScreen />} />
+      <Route path="/forgot-password" element={<ForgotPasswordScreen />} />
       <Route
         path="/home"
         element={
@@ -131,28 +110,24 @@ const App = () => {
         path="/explore"
         element={
           token ? (
-            <>
-              <div className="flex flex-col min-h-screen">
-                <Header handleToggleSidebar={handleToggleSidebar} />
-                <div className="flex text-xl flex-grow text-black">
-                  <Sidebar
-                    sidebar={sidebar}
-                    handleToggleSidebar={handleToggleSidebar}
-                    className="relative z-50"
-                  />
-                  <Container
-                    sidebar={sidebar}
-                    fluid
-                    className={`  transform duration-700    ${
-                      sidebar ? "ml-[250px]" : "ml-0"
-                    }`}
-                  >
-                    <ExploreScreen sidebar={sidebar} />
-                  </Container>
-                </div>
-                <Footer sidebar={sidebar} />
+            <div className="flex flex-col min-h-screen">
+              <Header handleToggleSidebar={handleToggleSidebar} />
+              <div className="flex text-xl flex-grow text-black">
+                <Sidebar
+                  sidebar={sidebar}
+                  handleToggleSidebar={handleToggleSidebar}
+                  className="relative z-50"
+                />
+                <Container
+                  sidebar={sidebar}
+                  fluid
+                  className={`transform duration-700 ${sidebar ? "ml-[250px]" : "ml-0"}`}
+                >
+                  <ExploreScreen sidebar={sidebar} />
+                </Container>
               </div>
-            </>
+              <Footer sidebar={sidebar} />
+            </div>
           ) : (
             <Navigate to="/login" />
           )
@@ -170,17 +145,14 @@ const App = () => {
           )
         }
       />
-
       <Route
         path="/certificationcenter"
         element={
           token ? (
-            <>
-              <div className="flex flex-col min-h-screen">
-                <CertificationCenter />
-                <Footer />
-              </div>
-            </>
+            <div className="flex flex-col min-h-screen">
+              <CertificationCenter />
+              <Footer />
+            </div>
           ) : (
             <Navigate to="/login" />
           )
@@ -200,7 +172,6 @@ const App = () => {
           }
         />
       )}
-
       <Route path="*" element={<Navigate to={token ? "/home" : "/login"} />} />
     </Routes>
   );

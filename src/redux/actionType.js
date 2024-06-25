d6b2aaf21a7e569df3618a18780974b8d10d5cb4
phibuinek <phibuinek@gmail.com>
2024-06-25 09:04:05 +0700

    update Role

--- src/redux/actionType.js
+++ src/redux/actionType.js
@@ -1,5 +1,6 @@
-export const LOGIN_REQUEST = 'LOGIN_REQUEST';
-export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
-export const LOGIN_FAILURE = 'LOGIN_FAILURE';
-export const LOGOUT = 'LOGOUT';
-export const SET_TOKEN = 'SET_TOKEN';
+export const LOGIN_REQUEST = "LOGIN_REQUEST";
+export const LOGIN_SUCCESS = "LOGIN_SUCCESS";
+export const LOGIN_FAILURE = "LOGIN_FAILURE";
+export const LOGOUT = "LOGOUT";
+export const SET_TOKEN = "SET_TOKEN";
+export const SET_ROLE = "SET_ROLE";

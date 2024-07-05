dbecac760aa725b5595ce7440322bfbf5a196d70
TranHieu0607 <133104245+TranHieu0607@users.noreply.github.com>
2024-07-05 16:32:29 +0700

    Review Page + API

--- src/pages/Review2.js
+++ src/pages/Review2.js
@@ -3,9 +3,8 @@ import { StarOutlined, SearchOutlined } from '@ant-design/icons';
 import { faStar } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-const Review2 = ({ sidebar }) => {
+const Review = ({ sidebar }) => {
     const [courses, setCourses] = useState([]);
-    const [searchTerm, setSearchTerm] = useState('');
 
     useEffect(() => {
         fetch('https://66806e6756c2c76b495c072b.mockapi.io/review')
@@ -14,29 +13,25 @@ const Review2 = ({ sidebar }) => {
             .catch(error => console.error('Error fetching data:', error));
     }, []);
 
-    const filteredCourses = courses.filter(course =>
-        course.name.toLowerCase().includes(searchTerm.toLowerCase())
-    );
-
     return (
         <div>
-            <div className={`mt-[100px] ml-6 text-black text-xl ${sidebar ? "w-[1400px]" : "w-[1640px]"}`}>
+            <div className={`mt-[100px] ml-6 text-black text-xl ${sidebar ? "w-[1200px]" : "w-[1400px]"}`}>
                 <h1><StarOutlined /> All Review</h1>
             </div>
-            <div className='flex gap-x-10 justify-center mt-8 ml-6'>
-                <div className={`bg-white h-[350px] mt-5 p-4 ${sidebar ? "w-[700px]" : "w-[900px]"}`}>
-                    <div className='text-2xl mt-4 ml-1'>
+            <div className="flex gap-x-10 justify-center mt-8 ml-6">
+                <div className={`bg-white h-[320px] mt-5 p-4 ${sidebar ? "w-[600px]" : "w-[700px]"}`}>
+                    <div className='text-xl mt-4 ml-1'>
                         <h1>All Student Feedback</h1>
                     </div>
-                    <div className='w-full h-14 bg-[#F7F7F7] border'>
-                        <div className='ml-6 pt-4 text-yellow-500 text-[25px] flex gap-3'>
-                            <h1 className='text-black mr-2 text-2xl'>4.6</h1>
+                    <div className= {` h-12 bg-[#F7F7F7] border ${sidebar ? "w-[550px]" : "w-[650px]"} `}>
+                        <div className='ml-6 pt-3 text-yellow-500 text-[20px] flex gap-1'>
+                            <h1 className='text-black mr-2'>4.6</h1>
                             <FontAwesomeIcon icon={faStar} />
                             <FontAwesomeIcon icon={faStar} />
                             <FontAwesomeIcon icon={faStar} />
                             <FontAwesomeIcon icon={faStar} />
                             <FontAwesomeIcon icon={faStar} />
-                            <p className='text-black text-[16px] ml-2 text-xl'>All Rating</p>
+                            <p className='text-black text-[16px] ml-2'>All Rating</p>
                         </div>
                     </div>
                     <div className='mt-5'>
@@ -102,17 +97,15 @@ const Review2 = ({ sidebar }) => {
                         </div>
                     </div>
                 </div>
-                <div className={`p-4 ${sidebar ? "w-[800px]" : "w-[900px]"}`}>
+                <div className={`p-4 ${sidebar ? "w-[600px]" : "w-[700px]"}`}>
                     <div>
                         <div className='flex items-center'>
-                            <h1 className='mt-1 mr-4 text-2xl'>All Reviews</h1>
+                            <h1 className='mt-1 mr-4'>All Reviews</h1>
                             <div className='flex items-center bg-[#F7F7F7] border-[#F7F7F7] relative w-[200px] ml-auto p-1'>
                                 <input
                                     type='text'
                                     placeholder='Search reviews..'
                                     className='focus:outline-none w-full text-xs p-2 bg-white text-black'
-                                    value={searchTerm}
-                                    onChange={e => setSearchTerm(e.target.value)}
                                 />
                                 <button className='bg-red-500 p-1.5 hover:bg-[#333333] transition duration-300'>
                                     <SearchOutlined style={{ color: 'white', fontSize: '16px' }} />
@@ -120,10 +113,11 @@ const Review2 = ({ sidebar }) => {
                             </div>
                         </div>
                     </div>
-                    {filteredCourses.map((course, index) => (
-                        <div key={index} className='bg-white mt-8 pb-4'>
+                    {/* Mapping over Courses array to display reviews */}
+                    {courses.map((course, index) => (
+                        <div key={index} className='bg-white w-full h-auto mt-8 pb-4'>
                             <div className='pt-4'>
-                                <h1 className='text-2xl ml-4'>Course Title Here</h1>
+                                <h1 className='text-xl ml-4'>Course Title Here</h1>
                                 <hr className="my-2 w-full border-gray-300" />
                             </div>
                             <div>
@@ -131,16 +125,16 @@ const Review2 = ({ sidebar }) => {
                                     <div>
                                         <img
                                             src={course.img}
-                                            width={60}
+                                            width={40}
                                             alt='img9'
                                         />
                                     </div>
                                     <div className="flex flex-col">
-                                        <h3 className='text-xl'>{course.name}</h3>
-                                        <div className="text-[18px] font-light text-gray-500">{course.time}</div>
+                                        <h3>{course.name}</h3>
+                                        <div className="text-sm font-light text-gray-500">{course.time}</div>
                                     </div>
                                 </div>
-                                <div className='ml-4 mt-4 text-yellow-500 text-[25px] flex gap-1'>
+                                <div className='ml-4 mt-4 text-yellow-500 text-[20px] flex gap-1'>
                                     <FontAwesomeIcon icon={faStar} />
                                     <FontAwesomeIcon icon={faStar} />
                                     <FontAwesomeIcon icon={faStar} />
@@ -148,7 +142,7 @@ const Review2 = ({ sidebar }) => {
                                     <FontAwesomeIcon className='text-[#F7F7F7]' icon={faStar} />
                                 </div>
                                 <div className='ml-4 mt-4 leading-7'>
-                                    <p className='text-gray-400 text-[19px]'>{course.content}</p>
+                                    <p className='text-gray-400 text-[14px]'>{course.content}</p>
                                 </div>
                             </div>
                         </div>
@@ -159,4 +153,4 @@ const Review2 = ({ sidebar }) => {
     );
 };
 
-export default Review2;
+export default Review;
